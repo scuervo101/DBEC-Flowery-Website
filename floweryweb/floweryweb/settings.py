4e47323903fca9e794277c3315ca114492e2be17
Samuel Cuervo <scuervo@ufl.edu>
2019-12-10 20:05:50 -0500

    Some Settings changes
    Tried adding our db to Django, not functional yet

--- floweryweb/floweryweb/settings.py
+++ floweryweb/floweryweb/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'floweryweb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'flowers2019.db'),
     }
 }
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
