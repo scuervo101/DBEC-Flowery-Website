20d27d58eb34471ca05c9a1cc6ea2ad449718095
Samuel Cuervo <scuervo@ufl.edu>
2019-12-12 18:25:29 -0500

    some sql stuff
    HTML sql connection, doesnt work

--- floweryweb/flowers/views.py
+++ floweryweb/flowers/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+import sqlite3
+
 def index(request):
+    
+    conn = sqlite3.connect('flowers2019.db')
+    c = conn.cursor()
+
+    query = "SELECT * FROM flowers"
+    c.execute(query)
+
+    listings = c.fetchall()
+
     context = {
+        'listings': listings
     }
-    return render(request, 'index.html', context=context)
+    return render(request, 'index.html', context)
